815df3c96fea10bdabea100ae02b6cdb9c4129a7
Nataorjuela <nataorjuela62@gmail.com>
2022-06-10 16:49:38 -0500

    diagrama de clases

--- countLine/src/main/java/arsw/app/App.java
+++ countLine/src/main/java/arsw/app/App.java
@@ -4,7 +4,7 @@ import arsw.app.operaciones.Phy;
 
 import java.io.File;
 /**
- *!
+ *
  *
  */
 public class App 
@@ -17,7 +17,7 @@ public class App
        System.out.println("la ruta del archivo es: "+ ruta);
        File documento = new File(ruta);
        Phy documentoPhy=new Phy(documento);
-        Loc documentoLoc = new Loc(documento);
+       Loc documentoLoc = new Loc(documento);
        if(tipoArchivo.equals("phy")){
            int totalPhy=documentoPhy.contarPhy(documento);
            System.out.println("total de lineas del archivo de tipo PHY= "+ totalPhy);
